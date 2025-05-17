4ba9d7a2a25e2ad78bb81191885c64a602ae753e
Juan Ignacio Carbajal <juanicarbajal@gmail.com>
2025-05-17 12:50:03 -0300

    Se agrega el proyecto

---
+++ tpProgra2/src/ar/edu/ungs/prog2/ticketek/Estadio.java
@@ -0,0 +1,16 @@
+package ar.edu.ungs.prog2.ticketek;
+
+public class Estadio extends Sede {
+
+	
+	
+	public Estadio(String nombre, String direccion, int capacidadMaxima) {
+		super(nombre, direccion, capacidadMaxima);
+
+	}
+	
+	
+	
+	
+
+}
---
+++ tpProgra2/src/ar/edu/ungs/prog2/ticketek/Sector.java
@@ -0,0 +1,17 @@
+package ar.edu.ungs.prog2.ticketek;
+
+public class Sector {
+
+	String nombre;
+	int capacidad;
+	int porcentajeAdicional;
+	
+	public Sector( String nombre, int capacidad, int porcentajeAdicional) {
+		
+		this.nombre = nombre;
+		this.capacidad = capacidad;
+		this.porcentajeAdicional = porcentajeAdicional;		
+		
+	}
+
+}
